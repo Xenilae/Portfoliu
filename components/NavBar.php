9936e01d59f84b7c96dc81b13044d62149bf63f9
Xenilae <gast777@yahoo.com>
2022-11-25 00:02:20 +0200

    changes

--- components/NavBar.php
+++ components/NavBar.php
@@ -2,10 +2,10 @@
   function NavBar() {
     return '
               <ul id="about">
-                  <li><a href="#Dascal" title="Who is dascal?" href="dascal.php">Who is dascal?</a></li>
-                  <li><a href="#Members" onclick='slowScroll("#Members")' title="Members">Members</a></li>
-                  <li><a href="#" onclick='slowScroll("#contacts")' title="Contacts" ><label class="btn" for="modal-1">Contacts</a></li>
-                  <li><a href="#Albums" onclick='slowScroll("#Albums")' title="Music/Albums">Music/Albums</a></li>
+                  <li><a href="dascal.php" title="Who is dascal?" href="dascal.php">Who is dascal?</a></li>
+                  <li><a href="#Members" title="Members">Members</a></li>
+                  <li><a href="#" title="Contacts" ><label class="btn" for="modal-1">Contacts</a></li>
+                  <li><a href="#Albums" title="Music/Albums">Music/Albums</a></li>
               </ul>
     ';
   }
